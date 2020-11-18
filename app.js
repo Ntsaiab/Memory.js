595cd54433ee31b8511344773643a394a68ef21e
Candice Sambury <candiland@candices-imac.home>
2020-11-17 19:00:03 -0500

    Pseudocode

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("Yes I'm connected");
+
+// create carousel pop up over board when game loads
+// user can click through carousel to read instructions
+// click begin game button.
+// user clicks first tile, tile reveals data
+    // start timer on first click
+    // create storage for data
+    // create a random generator to load data in tiles everytime page refreshes
+    // display value of first tile
+    // user clicks second tile, with first still visible
+    // if 1st tile and 2nd tile matches stay on screen
+    // if 1st tile and 2nd tile does not match return to base state after a specified time
+
+// create computer generated turn
+  // if 1st tile and 2nd tile matches stay on screen
+  // if 1st tile and 2nd tile does not match return to base state after a specified time
+
+// create a counter for when all the board is matched 
+    // user counter
+    // computer counter
+      // display as score
+
+// display You win message if user counter reaches x amount of points
+// display Computer wins if computer counter reaches x amount of points
+
+// 
+//display you lose message if time runs out 2nd time
+
