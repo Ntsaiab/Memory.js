131419565478d7d51a98835ff98ea5c25c4a9099
Candice Sambury <candiland@candices-imac.home>
2020-11-23 01:40:20 -0500

    Final beginner version

--- app.js
+++ app.js
@@ -31,32 +31,15 @@ console.log("Yes I'm connected");
 // ==============================
 //     Cached Dom Notes
 // ==============================
-// const mBoard = document.querySelector('.mBoard');
-const modal = document.querySelector('.modal');
-// const modal2 = document.querySelector(".modal2");
-const playGame = document.querySelector('.play-game');
-// const nGameBtn = document.querySelector("#ngame");
-// const rLevelBtn = document.querySelector("#rlevel");
-// const eGameBtn = document.querySelector("#egame");
-const tiles = document.querySelectorAll('.tile');
+
 const Msg = document.querySelector('.Msg');
+const modal = document.querySelector('.modal');
+const tiles = document.querySelectorAll('.tile');
+const playGame = document.querySelector('.play-game');
 const currStatsCon = document.querySelector('.stats');
 
 // ==============================
-//     Create Game Board
-// ==============================
-
-// THIS WORKS CREATES GAME BOARD
-// let num = 9;
-
-// for (let i = 1; i < num; i++) {
-//   let div = document.createElement("div");
-//   div.classList.add("tiles");
-//   mBoard.appendChild(div);
-// }
-
-// ==============================
-//     Create Game Play
+//   Create Stats for Game Play
 // ==============================
 class GamePlay {
   constructor (level, uscore, cscore, time) {
@@ -79,14 +62,15 @@ class GamePlay {
     `;
   }
 
+  // time function works just need to make it stop at 0
   timeUp () {
     setInterval(() => {
       this.time--;
-      this.updateStats();
       if (this.time === 0) {
         Msg.innerHTML = '<div class=\'Msg\'><span>Time is up! You lose!!</span></div>';
         console.log('Time is up! You lose!');
       }
+      this.updateStats();
     }, 5000);
   }
 
@@ -105,56 +89,23 @@ class GamePlay {
     this.updateStats();
   }
 }
-// ==============================
-//  Global Variables
-// ==============================
 
 const Stats = new GamePlay(1, 0, 0, 20);
-// let player1 = uscore;
-// let player2 = cscore;
+
 // ==============================
-//   Create Modal/EventLisFunc
+//   Create Modal
 // ==============================
 
-// THIS WORKS TOGGLES MODAL BEGINBTN AND NEWGAME BTN
+// THIS WORKS TOGGLES MODAL Start Learning BTN
 const toggleModal = () => {
   modal.classList.toggle('close');
   Stats.updateStats();
 };
-// // THIS DOES NOT WORK. ONLY POPS UP IN THE BIGINNING DOES NOT
-// // CLICK OFF.
-// const toggleModal2 = () => {
-//   // Maybe a modal as well saying thank you for Playing
-//   modal2.classList.toggle("open");
-// }
 
 // ==============================
 //   Data Source
 // ==============================
-// const dataSrc = [ // go with object look up methods.
-//   ['undefined', 'let x = ;'],
-//   ['function1', 'function x { };'],
-//   ['object1', '{ }'],
-//   ['array1', '[ ]']
-//   ['for loop', 'for(i = 0; i < 10; i++){}'],
-//   ['while loop', 'let i = 0; while(i < 10){i++;}']
-//   ['array2', 'const x = [];'],
-//   ['object2', 'const x = {}'],
-//   ['function2', 'const x = () => { };']
-// ]
 
-// // changed to object data source
-// const dataSrc = {
-//     gtUndefined: "let x = ;",
-//     function1: "function x {};",
-//     arrowFunction: "const x = () => {};",
-//     object1: "{}",
-//     object2: "const x = {}",
-//     array1: "[]",
-//     array2: "const x = [];",
-//     forLoop: "for(i = 0; i < 10; i++ {})",
-//     whileLoop: "let i = 0; while(i < 10) {i++}"
-// };
 const dataSrc = {
   tile1: {
     displayText: 'undefined',
@@ -196,65 +147,46 @@ const dataSrc = {
     displayText: 'whileLoop',
     pair: 5
 
+  },
+  tile11: {
+    displayText: '&&',
+    pair: 6
+  },
+  tile12: {
+    displayText: 'AND logical operator',
+    pair: 6
+
   }
 };
 
 // ==============================
-//     Create Computer Variables
+//   Functions/logic/Game Play
 // ==============================
-
-// ==============================
-//      Functions/logic/Game Play
-// ==============================
-
+// assigns game stat methods to a variable so it
+// can be used/called else where in the code
 const gameTime = () => Stats.timeUp();
+const levelUp = () => Stats.levelUp();
 const player1Score = () => Stats.player1Score();
 const player2Score = () => Stats.player2Score();
-const levelUp = () => Stats.levelUp();
 
-// DECIDED TO LEAVE RANDOM OUT FOR NOW
-// loop through random * element and sort. look up shuffle array
-// const random = Math.floor(Math.random()*dataSrc.length);
-// e.target.textContent = userInput;
-// if(random === random) {
-//   // mBoard.removeEventListener("click",)
-// }
-// let userInput = dataSrc[random];
-// e.target.textContent = userInput;
-
-// Figured out how to split key value pairs.
-
-// for (const [key, value] of Object.entries(dataSrc)) {
-//   mBoard.addEventListener("click", (e) => {
-//   console.log(`${(key)}`);
-//   // console.log(`${value}`);
-// });
-// }
-// const keys = Object.keys(dataSrc);
-
-// 1st click click counter outside of this scope increment inside clickcounter++
-// 2nd click check for match, if match update score then reset counter
-
+let turn = false;
+let selectionArr = [];
 const player1MatchArr = [];
 const player2MatchArr = [];
-let selectionArr = [];
-let turn = false;
 
-// console.log(tile);
 for (const tile of tiles) {
   // when box is clicked
   tile.addEventListener('click', (e) => {
-    // this starts the game timer on first click
+    // this starts the game timer and stats on first click
     gameTime();
     // if this tile is not in selection array and
     // if tile is not already matched in player1 array and
     // if tile is not already matched in player2 array
     if (!selectionArr.includes(dataSrc[tile.id]) &&
-        !player1MatchArr.includes(dataSrc[tile.id]) &&
-        !player2MatchArr.includes(dataSrc[tile.id])) {
+      !player1MatchArr.includes(dataSrc[tile.id]) &&
+      !player2MatchArr.includes(dataSrc[tile.id])) {
       // display the content
-      const word = dataSrc[tile.id].displayText;
-      tile.textContent = word;
+      tile.textContent = dataSrc[tile.id].displayText;
       // then push tile data into selection array
       selectionArr.push(dataSrc[tile.id]);
       // if there are at least two tiles in the selection array
@@ -262,17 +194,20 @@ for (const tile of tiles) {
         // then check match between the selected tiles
         // if selected tiles are a match
         if (selectionArr[0].pair === selectionArr[1].pair) {
-          // then push whole selection array into match array that corresponds with current player
+          // then push whole selection array into match array
+          // that corresponds with current player
           // empty the selection array
           if (turn === false) {
-            // pop and element from the selection array and push that element into the player1match array
+            // pop and element from the selection array and
+            // push that element into the player1match array
             // until the selection array is empty
             while (selectionArr.length !== 0) {
               player1MatchArr.push(selectionArr.pop());
               player1Score();
             }
           } else {
-            // pop and element from the selection array and push that element into the player1match array
+            // pop and element from the selection array and
+            // push that element into the player1match array
             // until the selection array is empty
             while (selectionArr.length !== 0) {
               player2MatchArr.push(selectionArr.pop());
@@ -282,46 +217,33 @@ for (const tile of tiles) {
           // toggle turn
           turn = !turn;
           // else empty the selection array
+          // *go through selected tiles and
+          // reset tiles in selection array*
         } else {
           selectionArr = [];
-          setTimeout(() => {
-            tile.textcontent = '';
-          }, 500);
           turn = !turn;
         }
         // end if
       }
-    // end if
+      // end if
     }
-    if ((player1MatchArr.length + player2MatchArr.length) === 10) {
-      levelUp();
+    // check if there is a winner
+    // if all tiles or total points/elements in a array - win condition
+    if ((player1MatchArr.length + player2MatchArr.length) === 12) {
       if (player1MatchArr.length > player2MatchArr.length) {
-        console.log('Player One Wins!');
         Msg.innerHTML = '<div class=\'Msg\'><span>Player One Wins!</span></div>';
       } else if (player1MatchArr.length < player2MatchArr.length) {
-        console.log('Player Two Wins!');
         Msg.innerHTML = '<div class=\'Msg\'><span>Player Two Wins!</span></div>';
       } else {
         Msg.innerHTML = '<div class=\'Msg\'><span>It\'s a tie!</span></div>';
-        console.log("It's a tie!");
       }
+      levelUp();
     }
-
-    // console.log('selection', selectionArr);
-    // console.log('player1', player1MatchArr);
-    // console.log('player2', player2MatchArr);
   });
 }
-// tasks
-// check if there is a winner
-// if all tiles or total points/elements in a array - win condition
-// go through selected tiles and reset tiles in selection array.
+
 // ==============================
 //     Event Listeners
 // ==============================
 
 playGame.addEventListener('click', toggleModal);
-
-// nGameBtn.addEventListener("click", toggleModal);
-// // rLevelBtn.addEventListener("click",);
-// eGameBtn.addEventListener("click", toggleModal2);
